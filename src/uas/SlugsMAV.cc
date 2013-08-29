cf7a78383e2fc15c7c7afd7363a920469c8a01fd
David Goodman <dagoodma@gmail.com>
2013-08-29 15:11:21 -0700

    Removed old commented out code.

--- src/uas/SlugsMAV.cc
+++ src/uas/SlugsMAV.cc
@@ -6,41 +6,6 @@ SlugsMAV::SlugsMAV(MAVLinkProtocol* mavlink, int id) :
     UAS(mavlink, id)
 {
     qDebug() << "Spawning a SLUGS MAV.";
-    /*
-    widgetTimer = new QTimer (this);
-    widgetTimer->setInterval(SLUGS_UPDATE_RATE);
-
-    connect (widgetTimer, SIGNAL(timeout()), this, SLOT(emitSignals()));
-    widgetTimer->start();
-*/
-    /*
-    memset(&mlRawImuData ,0, sizeof(mavlink_raw_imu_t));// clear all the state structures
-
-#ifdef MAVLINK_ENABLED_SLUGS
-
-
-    memset(&mlGpsData, 0, sizeof(mavlink_gps_raw_t));
-    memset(&mlCpuLoadData, 0, sizeof(mavlink_cpu_load_t));
-    memset(&mlAirData, 0, sizeof(mavlink_air_data_t));
-    memset(&mlSensorBiasData, 0, sizeof(mavlink_sensor_bias_t));
-    memset(&mlDiagnosticData, 0, sizeof(mavlink_diagnostic_t));
-    memset(&mlBoot ,0, sizeof(mavlink_boot_t));
-    memset(&mlGpsDateTime ,0, sizeof(mavlink_gps_date_time_t));
-    memset(&mlApMode ,0, sizeof(mavlink_set_mode_t));
-    memset(&mlNavigation ,0, sizeof(mavlink_slugs_navigation_t));
-    memset(&mlDataLog ,0, sizeof(mavlink_data_log_t));
-    memset(&mlPassthrough ,0, sizeof(mavlink_ctrl_srfc_pt_t));
-    memset(&mlActionAck,0, sizeof(mavlink_action_ack_t));
-    memset(&mlAction ,0, sizeof(mavlink_slugs_action_t));
-
-    memset(&mlScaled ,0, sizeof(mavlink_scaled_imu_t));
-    memset(&mlServo ,0, sizeof(mavlink_servo_output_raw_t));
-    memset(&mlChannels ,0, sizeof(mavlink_rc_channels_raw_t));
-
-    updateRoundRobin = 0;
-    uasId = id;
-#endif
-*/
 }
 
 
@@ -188,41 +153,6 @@ void SlugsMAV::receiveMessage(LinkInterface* link, mavlink_message_t message)
                                          (double)mavlink_msg_mid_lvl_cmds_get_rCommand(&message));
             break;
         } // switch
-
-
-        /*
-
-        case MAVLINK_MSG_ID_RC_CHANNELS_RAW:
-            mavlink_msg_rc_channels_raw_decode(&message, &mlChannels);
-            break;
-
-            switch (mlAction.actionId) {
-            case SLUGS_ACTION_EEPROM:
-                if (mlAction.actionVal == SLUGS_ACTION_FAIL) {
-                    emit textMessageReceived(message.sysid, message.compid, 255, "EEPROM Write Fail, Data was not saved in Memory!");
-                }
-                break;
-
-            case SLUGS_ACTION_PT_CHANGE:
-                if (mlAction.actionVal == SLUGS_ACTION_SUCCESS) {
-                    emit textMessageReceived(message.sysid, message.compid, 0, "Passthrough Succesfully Changed");
-                }
-                break;
-
-            case SLUGS_ACTION_MLC_CHANGE:
-                if (mlAction.actionVal == SLUGS_ACTION_SUCCESS) {
-                    emit textMessageReceived(message.sysid, message.compid, 0, "Mid-level Commands Succesfully Changed");
-                }
-                break;
-            }
-
-            //break;
-
-        default:
-            //        qDebug() << "\nSLUGS RECEIVED MESSAGE WITH ID" << message.msgid;
-            break;
-        }
-        */
 #endif
     } // (message.sysid == uasId)
 }
@@ -275,87 +205,3 @@ void SlugsMAV::setPassthroughSurfaces(bool throttle, bool ailerons, bool rudder,
     sendMessage(msg);
 #endif
 }
-
-/*
-void SlugsMAV::emitSignals (void)
-{
-#ifdef MAVLINK_ENABLED_SLUGS
-    switch(updateRoundRobin) {
-    case 1:
-        emit slugsCPULoad(uasId, mlCpuLoadData);
-        emit slugsSensorBias(uasId,mlSensorBiasData);
-        break;
-
-    case 2:
-        emit slugsAirData(uasId, mlAirData);
-        emit slugsDiagnostic(uasId,mlDiagnosticData);
-
-        break;
-
-    case 3:
-        emit slugsNavegation(uasId, mlNavigation);
-        emit slugsDataLog(uasId, mlDataLog);
-        break;
-
-    case 4:
-        emit slugsGPSDateTime(uasId, mlGpsDateTime);
-
-        break;
-
-    case 5:
-        emit slugsActionAck(uasId,mlActionAck);
-        emit emitGpsSignals();
-        break;
-
-    case 6:
-        emit slugsChannels(uasId, mlChannels);
-        emit slugsServo(uasId, mlServo);
-        emit slugsScaled(uasId, mlScaled);
-
-        break;
-    }
-
-    emit slugsAttitude(uasId, mlAttitude);
-    emit attitudeChanged(this,
-                         mlAttitude.roll,
-                         mlAttitude.pitch,
-                         mlAttitude.yaw,
-                         0.0);
-#endif
-
-    emit slugsRawImu(uasId, mlRawImuData);
-
-
-    // wrap around
-    updateRoundRobin = updateRoundRobin > 10? 1: updateRoundRobin + 1;
-
-
-}
-*/
-
-/*
-
-#ifdef MAVLINK_ENABLED_SLUGS
-void SlugsMAV::emitGpsSignals (void)
-{
-
-    // qDebug()<<"After Emit GPS Signal"<<mlGpsData.fix_type;
-
-
-    //ToDo Uncomment if. it was comment only to test
-
-// if (mlGpsData.fix_type > 0){
-    emit globalPositionChanged(this,
-                               mlGpsData.lon,
-                               mlGpsData.lat,
-                               mlGpsData.alt,
-                               0.0);
-
-    emit slugsGPSCogSog(uasId,mlGpsData.hdg, mlGpsData.v);
-
-}
-
-
-
-#endif // MAVLINK_ENABLED_SLUGS
-*/
